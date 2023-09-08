d341da492385703e96cfdb21a62b6a005a197038
Rakibul Hasan <shurovmohammad@gmail.com>
2023-09-08 09:52:39 -0700

    3rd Commit (chart)

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,12 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('home/', show_data, name = "home"),
     path('', show_data),
+    path('home/', show_data, name = "home"),
     path('delete/<int:id>/', delete_data, name="delete"),
-    path('update/<int:id>/', update_data, name="update")
+    path('update/<int:id>/', update_data, name="update"),
+    path('select_chart/', chart_view, name="chart_view"),
+    path('line_chart_view/', line_chart_view, name="line_chart_view"),
+    path('bar_chart_view/', bar_chart_view, name="bar_chart_view"),
    
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,15 @@
-from django.forms import modelform_factory
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import  redirect, render
 import json
 from core.forms import InformationForm
 from .models import Information
 
 
+
+
 #view for display json data
 def show_data(request):
-    objs = Information.objects.filter(id__range=["1", "50"])
+    objs = Information.objects.filter(id__range=["1", "200"])
     return render(request,"core/show.html",{'objs':objs})            
 
 
@@ -35,6 +36,33 @@ def update_data(request,id):
         
     return render(request,'core/update.html',{'form':fm})
         
+
+
+
+#chart select
+def chart_view(request):
+    return render(request, 'core/visualization.html')
+
+
+#bar chart
+def bar_chart_view(request):
+    data = Information.objects.all().order_by('date')
+    context = {
+        'chart_data': data,
+        
+    }
+    return render(request, 'core/bar.html', context)
+
+
+#line chart
+def line_chart_view(request):
+    data = Information.objects.all().order_by('date')
+    context = {
+        'chart_data': data,
+        
+    }
+    return render(request, 'core/line.html', context)
+
 
 
 
